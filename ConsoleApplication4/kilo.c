59c26736738feb20b4e8d2b53b94a5bfb3030789
chenweigao <297859260@qq.com>
2018-11-15 15:37:15 +0800

    add function: HOME and END key

--- ConsoleApplication4/kilo.c
+++ ConsoleApplication4/kilo.c
@@ -19,6 +19,8 @@ enum editorKey
 	ARROW_RIGHT,
 	ARROW_UP,
 	ARROW_DOWN,
+	HOME_KEY,
+	END_KEY,
 	PAGE_UP,
 	PAGE_DOWN
 };
@@ -85,8 +87,12 @@ int editorReadKey() {
 					// pageup and pagedown <esc>[5~ and <esc>[6~
 					switch (seq[1])
 					{
+					case '1': return HOME_KEY;
+					case '4': return END_KEY;
 					case '5': return PAGE_UP;
 					case '6': return PAGE_DOWN;
+					case '7': return HOME_KEY;
+					case '8': return END_KEY;
 					}
 				}
 			}
@@ -98,7 +104,16 @@ int editorReadKey() {
 				case 'B': return ARROW_DOWN;
 				case 'C': return ARROW_RIGHT;
 				case 'D': return ARROW_LEFT;
+				case 'H': return HOME_KEY;
+				case 'F': return END_KEY;
 				}
+			}
+		}
+		else if (seq[0] == '0')
+		{
+			switch (seq[1]) {
+			case 'H': return HOME_KEY;
+			case 'F': return END_KEY;
 			}
 		}
 
@@ -285,6 +300,12 @@ void editorProcessKeypress() {
 		write(STDOUT_FILENO, "\x1b[2J", 4);
 		write(STDOUT_FILENO, "\x1b[H", 3);
 		exit(0);
+		break;
+	case HOME_KEY:
+		E.cx = 0;
+		break;
+	case END_KEY:
+		E.cx = E.screencols - 1;
 		break;
 
 	case PAGE_UP:
