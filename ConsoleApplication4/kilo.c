0f783ddbf8c910b7d03476832f93efd3ea791bb7
chenweigao <297859260@qq.com>
2018-11-18 22:45:51 +0800

    moving letf at the start of a line

--- ConsoleApplication4/kilo.c
+++ ConsoleApplication4/kilo.c
@@ -351,6 +351,11 @@ void editorMoveCursor(int key) {
 		{
 			E.cx--;
 		}
+		else if (E.cy > 0)
+		{
+			E.cy--;
+			E.cx = E.row[E.cy].size;
+		}
 		break;
 	case ARROW_RIGHT:
 		if (row && E.cx < row->size)
@@ -370,6 +375,12 @@ void editorMoveCursor(int key) {
 			E.cy++;
 		}
 		break;
+	}
+
+	row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+	int rowlen = row ? row->size : 0;
+	if (E.cx > rowlen) {
+		E.cx = rowlen;
 	}
 }
 
