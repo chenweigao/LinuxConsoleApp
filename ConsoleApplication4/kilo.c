8649c8b91d60609301cc10ae566a53fdb4aaa69c
chenweigao <297859260@qq.com>
2018-11-18 22:01:35 +0800

    horizontal scorlling

--- ConsoleApplication4/kilo.c
+++ ConsoleApplication4/kilo.c
@@ -42,6 +42,7 @@ struct editorConfig
 {
 	int cx, cy; // cursor position
 	int rowoff;
+	int coloff;
 	int screenrows;
 	int screencols;
 	int numrows; // 0 or 1
@@ -256,6 +257,12 @@ void editorScorll() {
 	if (E.cy >= E.rowoff + E.screenrows) {
 		E.rowoff = E.cy - E.screenrows + 1;
 	}
+	if (E.cx < E.coloff) {
+		E.coloff = E.cx;
+	}
+	if (E.cx > E.coloff + E.screencols) {
+		E.coloff = E.cx - E.screencols + 1;
+	}
 }
 
 void editorDrawRows(struct abuf *ab) {
@@ -290,9 +297,10 @@ void editorDrawRows(struct abuf *ab) {
 		}
 		else
 		{
-			int len = E.row[filerow].size;
+			int len = E.row[filerow].size - E.coloff;
+			if (len < 0) len = 0;
 			if (len > E.screencols) len = E.screencols;
-			abAppend(ab, E.row[filerow].chars, len);
+			abAppend(ab, &E.row[filerow].chars[E.coloff], len);
 		}
 
 		abAppend(ab, "\x1b[K", 3);
@@ -320,7 +328,7 @@ void editorRefreshScreen() {
 	editorDrawRows(&ab);
 
 	char buf[32];
-	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
 	abAppend(&ab, buf, strlen(buf));
 
 	//write(STDOUT_FILENO, "\x1b[H", 3);
@@ -343,10 +351,7 @@ void editorMoveCursor(int key) {
 		}
 		break;
 	case ARROW_RIGHT:
-		if (E.cx != E.screencols - 1)
-		{
-			E.cx++;
-		}
+		E.cx++;
 		break;
 	case ARROW_UP:
 		if (E.cy != 0)
@@ -406,6 +411,7 @@ void initEditor() {
 	E.numrows = 0;
 	E.row = NULL;
 	E.rowoff = 0;
+	E.coloff = 0;
 
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
